c22837574c5646f2b1af41449df041e44c6af500
Adrian LaCour <adrianj.lacour@gmail.com>
2019-10-21 21:37:33 -0500

    Updates

---
+++ precache-manifest.9a9e907b019d69b4aa9cf96a059f14db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10aefadfacb0279855c585c063db6fb8",
+    "url": "/Doogo-Pals/index.html"
+  },
+  {
+    "revision": "7876fd801109d563ebcc",
+    "url": "/Doogo-Pals/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d8eca0b5c6743300851c",
+    "url": "/Doogo-Pals/static/css/main.70846e27.chunk.css"
+  },
+  {
+    "revision": "7876fd801109d563ebcc",
+    "url": "/Doogo-Pals/static/js/2.b9edcb38.chunk.js"
+  },
+  {
+    "revision": "d8eca0b5c6743300851c",
+    "url": "/Doogo-Pals/static/js/main.90c82534.chunk.js"
+  },
+  {
+    "revision": "588cfa59671d1f8d8d0f",
+    "url": "/Doogo-Pals/static/js/runtime-main.3c65194e.js"
+  }
+]);
